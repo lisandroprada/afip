a2bfb859f8bc92c399110812cc400aa2c2bd25ba
lisandroprada <lisandro.prada@gmail.com>
2021-11-04 12:50:10 -0300

    Facturas

--- getLastVoucher.js
+++ getLastVoucher.js
@@ -5,10 +5,14 @@ const afip = new Afip({CUIT: 33710943619});
 // const lastVoucher = await afip.ElectronicBilling.getLastVoucher(1, 6); //Devuelve el número del último comprobante creado para el punto de venta 1 y el tipo de comprobante 6 (Factura B)
 // console.log(lastVoucher);
 
-afip.ElectronicBilling.getSalesPoints().then((res) => console.log(res));
+// afip.ElectronicBilling.getSalesPoints().then((res) => console.log(res));
 
-afip.ElectronicBilling.getLastVoucher(5, 6).then((res) => console.log(res));
+// afip.ElectronicBilling.getLastVoucher(5, 6).then((res) => console.log(res));
 
-// afip.ElectronicBilling.getVoucherInfo(1, 1, 6).then((res) => {
-//   console.log(res);
+// afip.ElectronicBilling.getVoucherInfo(5450, 5, 6).then((res) => {
+//   //   console.log(res);
+//   console.log(res.Iva);
 // });
+
+// afip.ElectronicBilling.getDocumentTypes().then((res) => console.log(res));
+afip.ElectronicBilling.getVoucherTypes().then((res) => console.log(res));
